deb39dbe87fe294c2f89576902724eb9ccd157d6
Youngrae Cho <ip323@naver.com>
2024-11-03 22:17:22 +0900

    Jump

--- 2d-game-practice/Assets/Scripts/Player.cs
+++ 2d-game-practice/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,15 @@ public class Player : MonoBehaviour
     // MonoBehaviour를 상속 받았을 경우
     // gameObject, transform 변수 가지고 있다.
     public float speed;
+    public float jumpPower;
+    public bool isJump = false;
+    public Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         // Input.GetKey (키가 눌려있는 동안)
@@ -21,14 +31,29 @@ public class Player : MonoBehaviour
             
             // Translate(x,y,z) -> Object를 x,y,z만큼 이동시킨다.
             // Time.deltaTime -> 1초를 프레임으로 나눈 값 -> 모든 컴퓨터 프레임에서 동일한 설정값으로 동작하게끔 (1초에 60번)
-            transform.Translate(speed * Time.deltaTime, 0, 0);
-            transform.localScale = new Vector3(5.0f, transform.localScale.y, transform.localScale.z);
+            rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
+            GetComponent<SpriteRenderer>().flipX = false;
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             // transform.position += new Vector3(-speed, 0.0f, 0.0f);
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
-            transform.localScale = new Vector3(-5.0f, transform.localScale.y, transform.localScale.z);
+            rigidBody.velocity = new Vector2(-speed, rigidBody.velocity.y);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0.0f, rigidBody.velocity.y);
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Space) && isJump == false)
+        {
+            isJump = true;
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpPower);
+        }
+
+        if (rigidBody.velocity.y == 0.0f)
+        {
+            isJump = false;
         }
     }
 }
